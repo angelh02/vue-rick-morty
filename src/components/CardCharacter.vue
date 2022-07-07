94cbb99034b7a1d0ea2450a4bcd937ede8de0f42
angelh02 <angelhurtadom02@gmail.com>
2022-07-06 21:43:31 -0500

    task: adding card character component to show the character info

---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="character">
+      <img :src="character.image" :alt="character.name"/>
+      <div class="character__info">
+          {{ character.name }}
+          <div class="status">
+              <span :class="
+                character.status == 'Alive' ? 'alive' : (
+                    character.status == 'Death' ? 'dead' : 'default'
+                )
+              "></span>
+              <span>{{character.status}} - {{character.species}}</span>
+          </div>
+          <div class="origin">
+              <span>
+                  Origin : {{character.origin.name}}
+              </span>
+          </div>
+          <div class="location">
+              <span>
+                  Location : {{character.location.name}}
+              </span>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props : ['character']
+}
+</script>
+
+<style>
+
+</style>
